858bc34461a28195788b08341e9c83ebaad4a4f6
Saleemahmh <ssaleemah24@gmail.com>
2017-08-10 11:29:18 +0530

    War file Updation

--- src/main/java/com/sag/person/PersonApplication.java
+++ src/main/java/com/sag/person/PersonApplication.java
@@ -2,10 +2,15 @@ package com.sag.person;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages="com.sag.person")
-public class PersonApplication {
-
+public class PersonApplication extends SpringBootServletInitializer {
+	  @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(PersonApplication.class);
+	    }
 	public static void main(String[] args) {
 		SpringApplication.run(PersonApplication.class, args);
 	}
